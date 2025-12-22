fb97be7937c6b9f5edd34ff70fa77c44ea2ce846
Jaber <jaberpatwary0@gmail.com>
2025-12-22 20:10:00 +0600

    chag

---
+++ src/router/router.go
@@ -0,0 +1,32 @@
+package router
+
+import (
+	"app/src/config"
+	"app/src/service"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func Routes(app *fiber.App, db *gorm.DB) {
+
+	UserService := service.NewUserService(db)
+	CommentService := service.NewCommentService(db)
+	ComService := service.NewComService(db)
+	AdminService := service.NewAdminService(db)
+
+	v1 := app.Group("/v1")
+
+	//HealthCheckRoutes(v1, healthCheckService)
+
+	UserRoutes(v1, UserService)
+	CommentRoutes(v1, CommentService)
+	ComRoutes(v1, ComService)
+	AdminRoutes(v1, AdminService)
+
+	// TODO: add another routes here...
+
+	if !config.IsProd {
+		DocsRoutes(v1)
+	}
+}
